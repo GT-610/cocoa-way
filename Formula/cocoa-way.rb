28a81fc27fe536970023788f4e14072c64ef502a
J-x-Z <39978180+J-x-Z@users.noreply.github.com>
2026-07-18 15:28:52 +0800

    Point Homebrew at 2.0.2

--- Formula/cocoa-way.rb
+++ Formula/cocoa-way.rb
@@ -1,8 +1,8 @@
 class CocoaWay < Formula
   desc "Native macOS Wayland compositor for running Linux apps"
   homepage "https://github.com/J-x-Z/cocoa-way"
-  url "https://github.com/J-x-Z/cocoa-way/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "83a94f1317b181f756a6c7b2d710e8a76c776bca7081feb6405e313e841eac72"
+  url "https://github.com/J-x-Z/cocoa-way/archive/refs/tags/v2.0.2.tar.gz"
+  sha256 "4676a3cdead8ecbd20b37311c8ba671ad1c17fd7fa629a396ca4683edc373222"
   license "GPL-3.0-only"
   head "https://github.com/J-x-Z/cocoa-way.git", branch: "main"
 
